9d0d04c594bce518c4f35e3fe79396fb7cbd499a
Andrew Eells <aeells@users.noreply.github.com>
2022-12-20 22:00:24 -0800

    Adding example Rest API and WAF trigger to create SSM param.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -1,12 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
 import {Construct} from 'constructs';
+import * as path from 'path';
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {ApiGatewayWafTrigger} from "./api-gateway-waf-trigger";
 
 export class RestApiStack extends cdk.Stack
 {
+    private restApi: apigateway.RestApi;
+    
     constructor(scope: Construct, id: string, props?: cdk.StackProps)
     {
         super(scope, id, props);
+
+        // in the real world, our api endpoint resources are loaded from an external package
+        this.createExampleRestApi();
+
+        // there is no cdk api to list/get RestApi resources so use a lambda and sdk
+
+        const apiGatewayWafTrigger = new ApiGatewayWafTrigger(this, 'ApiGatewayWafTrigger', {
+            restApiId: this.restApi.restApiId,
+            stageName: 'prod',
+        });
+
+        apiGatewayWafTrigger.triggerFunction.executeAfter(this.restApi);
+    }
+
+    private createExampleRestApi()
+    {
+        this.restApi = new apigateway.RestApi(this, 'ApiGatewayRestApi', {
+            description: 'example api gateway',
+        });
+
+        const users = this.restApi.root.addResource('users');
+        const user = users.addResource('{userId}');
+        const getUserLambda = new lambda.Function(this, 'getUserLambda', {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            handler: 'index.main',
+            code: lambda.Code.fromAsset(path.join(__dirname, '/user')),
+        });
+        user.addMethod('GET', new apigateway.LambdaIntegration(getUserLambda, {proxy: true}));
+        // ...etc...
     }
 }
---
+++ lib/waf/index.ts
@@ -0,0 +1,26 @@
+import * as AWS from 'aws-sdk';
+
+export const handler = async (
+    event:  any
+) => {
+    const apiExport : any = await new AWS.APIGateway().getExport({
+        exportType: 'oas30',
+        restApiId: `${process.env.REST_API_ID}`,
+        stageName: `${process.env.STAGE_NAME}`,
+        accepts: 'application/json'
+    }).promise();
+
+    const openApiSpec: string[] = [];
+    for (const [key] of Object.entries(JSON.parse(apiExport.body.toString()).paths))
+    {
+        openApiSpec.push(key.replace('{userId}', '[A-Za-z0-9]+-[A-za-z0-9]+'));
+    }
+
+    await new AWS.SSM().putParameter({
+        Name: 'openapi-paths',
+        Value: openApiSpec.toString(),
+        Overwrite: true,
+        Type: 'String'
+    }).promise();
+};
+
